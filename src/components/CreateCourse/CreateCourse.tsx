dcb3e96d033398fda32309987c0658abce2b3544
nick722 <79ZUdtd3orzC!GeTcb^s&79jBB>
2023-05-22 19:44:11 +0300

    WIP CreateCourse

--- src/components/CreateCourse/CreateCourse.tsx
+++ src/components/CreateCourse/CreateCourse.tsx
@@ -1,7 +1,20 @@
 import React from 'react';
+import Input from '../common/Input/Input';
 
 const CreateCourse = () => {
-	return <div>CreateCourse component</div>;
+	return (
+		<div>
+			<h2>CreateCourse component </h2>
+			Title <Input labelText={} placeholderText={} onChange={} />
+			<div>Description (textarea)</div>
+			<div>List of authors</div>
+			<div>
+				Course authors - contains a list of authors course and their
+				corresponding Delete author buttons
+			</div>
+			<div>Create author </div>
+		</div>
+	);
 };
 
 export default CreateCourse;
