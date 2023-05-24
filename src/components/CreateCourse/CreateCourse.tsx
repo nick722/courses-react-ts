1563b392fd2848a0cbeade0a5d8e5d989dab2fb7
Nikolai Iakushchenko <nikolai_iakushchenko@epam.com>
2023-05-24 14:14:50 +0300

    add AddAuthor.tsx

--- src/components/CreateCourse/CreateCourse.tsx
+++ src/components/CreateCourse/CreateCourse.tsx
@@ -2,11 +2,13 @@ import React from 'react';
 import Input from '../common/Input/Input';
 import Button from '../common/Button/Button';
 
-import './CreateCourse.scss';
-import AuthorItem from './components/AuthorItem';
 import Authors from './components/Authors/Authors';
-import { Author } from '../../types';
 import CourseAuthors from './components/CourseAuthors/CourseAuthors';
+import AddAuthor from './components/AddAuthor/AddAuthor';
+
+import { Author } from '../../types';
+
+import './CreateCourse.scss';
 
 const TITLE = 'Title';
 const TITLE_PLACEHODER = 'Enter title...';
@@ -40,9 +42,15 @@ const CreateCourse = ({ authors }: CreateCourseProps) => {
 				<label for='textarea'>Description</label>
 				<textarea id='textarea' placeholder={DESCRIPTION_PLACEHOLDER} />
 			</div>
-			<Authors authors={authors} />
-			<CourseAuthors />
-			<div>Create author </div>
+			<div className='create-course__main'>
+				<div className='create-course__left-panel'>
+					<AddAuthor />
+				</div>
+				<div className='create-course__right-panel'>
+					<Authors authors={authors} />
+					<CourseAuthors />
+				</div>
+			</div>
 		</div>
 	);
 };
---
+++ src/components/CreateCourse/components/AddAuthor/AddAuthor.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import './AddAuthor.scss';
+
+const AddAuthor = () => {
+	return (
+		<div className='add-author'>
+			<h3>Add author</h3>
+		</div>
+	);
+};
+
+export default AddAuthor;
