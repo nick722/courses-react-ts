20d4b9afd62f89343ffb946a32c1d4f5728db4c4
Nikolai Iakushchenko <nikolai_iakushchenko@epam.com>
2023-05-24 15:14:42 +0300

    style CreateAuthor component

--- src/components/CreateCourse/components/CreateAuthor/CreateAuthor.tsx
+++ src/components/CreateCourse/components/CreateAuthor/CreateAuthor.tsx
@@ -1,11 +1,30 @@
 import React from 'react';
 
 import './CreateAuthor.scss';
+import Input from '../../../common/Input/Input';
+import Button from '../../../common/Button/Button';
+
+const INPUT_TEXT = 'Author name';
+const INPUT_PLACEHOLDER = 'Enter author name...';
+const BUTTON_TEXT = 'Create author';
 
 const CreateAuthor = () => {
 	return (
 		<div className='add-author'>
 			<h3>Create author</h3>
+			<Input
+				labelText={INPUT_TEXT}
+				placeholderText={INPUT_PLACEHOLDER}
+				onChange={() => {
+					/**/
+				}}
+			/>
+			<Button
+				buttonText={BUTTON_TEXT}
+				onClick={() => {
+					/**/
+				}}
+			/>
 		</div>
 	);
 };
