d0de73f1efea01bab14d9915e97ad0a54869b228
Nikolai Iakushchenko <nikolai_iakushchenko@epam.com>
2023-05-24 13:55:22 +0300

    style Authors

--- src/components/CreateCourse/components/Authors/Authors.tsx
+++ src/components/CreateCourse/components/Authors/Authors.tsx
@@ -2,17 +2,22 @@ import React from 'react';
 import { Author } from '../../../../types';
 import AuthorItem from '../AuthorItem';
 
+import './Authors.scss';
+
 interface AuthorsProps {
 	authors: Author[];
 }
 
 const Authors = ({ authors }: AuthorsProps) => {
 	return (
-		<ul>
-			{authors.map((author) => (
-				<AuthorItem authorsName={author.name} />
-			))}
-		</ul>
+		<div className='authors'>
+			<h3 className='authors__title'>Authors</h3>
+			<ul>
+				{authors.map((author) => (
+					<AuthorItem authorsName={author.name} />
+				))}
+			</ul>
+		</div>
 	);
 };
 
