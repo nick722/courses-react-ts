297efa5d7adcea236f39ca3857fdf7ff6383eef8
Nikolai Iakushchenko <nikolai_iakushchenko@epam.com>
2023-05-26 18:58:18 +0300

    add structure and style to CourseInfo

--- src/components/CourseInfo/CourseInfo.tsx
+++ src/components/CourseInfo/CourseInfo.tsx
@@ -1,7 +1,25 @@
 import React from 'react';
 
+import './CourseInfo.scss';
+
 const CourseInfo = () => {
-	return <div>CourseInfo component</div>;
+	return (
+		<div className='course-info'>
+			<p> &lt; Back to courses</p>
+			<h1>Title</h1>
+			<div className='course-info__main'>
+				<div className='course-info__left-side'>
+					<p>Description</p>
+				</div>
+				<div className='course-info__right-side'>
+					<p>ID: </p>
+					<p>Duration: </p>
+					<p>Created: </p>
+					<p>Authors:</p>
+				</div>
+			</div>
+		</div>
+	);
 };
 
 export default CourseInfo;
