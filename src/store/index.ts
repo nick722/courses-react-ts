7d724136e62e19176acd5e08be21e9fb8f11ae49
Nikolai Iakushchenko <nikolai_iakushchenko@epam.com>
2023-05-28 11:25:34 +0300

    more store to index.ts

--- src/store/index.ts
+++ src/store/index.ts
@@ -7,3 +7,10 @@
 // const store = createStore(rootReducer, appInitialStore);
 //
 // export default store;
+
+import { configureStore } from '@reduxjs/toolkit';
+import rootReducer from './rootReducer';
+
+const store = configureStore({ reducer: rootReducer });
+
+export default store;
--- src/store/rootReducer.ts
+++ src/store/rootReducer.ts
@@ -1,8 +1,8 @@
-import { combineReducers, configureStore } from '@reduxjs/toolkit';
+import { combineReducers } from '@reduxjs/toolkit';
 import { coursesReducer } from './courses/reducer';
 
 const rootReducer = combineReducers({
 	courses: coursesReducer,
 });
 
-export const store = configureStore({ reducer: rootReducer });
+export default rootReducer;
