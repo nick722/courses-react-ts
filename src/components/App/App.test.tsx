6648904ca493877848befd7d320fdf0658fecd3a
Nikolai Iakushchenko <nikolai_iakushchenko@epam.com>
2023-05-28 22:06:19 +0300

    WIP add test for App

--- src/components/App/App.test.tsx
+++ src/components/App/App.test.tsx
@@ -2,8 +2,16 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-	render(<App />);
-	const linkElement = screen.getByText(/learn react/i);
-	expect(linkElement).toBeInTheDocument();
+// test('renders learn react link', () => {
+// 	render(<App />);
+// 	// const linkElement = screen.getByText(/learn react/i);
+// 	// expect(linkElement).toBeInTheDocument();
+// });
+
+describe('App', () => {
+	it('should display course title', async () => {
+		render(<App />);
+
+		screen.debug();
+	});
 });
