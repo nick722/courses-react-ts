73df98a9595068f7d03f4d2b7f19c4062a8993e4
Nikolai Iakushchenko <nikolai_iakushchenko@epam.com>
2023-05-28 21:51:31 +0300

    move App.tsx to components folder

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,18 +1,18 @@
 import React, { useEffect, useState } from 'react';
-import Header from './components/Header/Header';
-import CreateCourse from './components/CreateCourse/CreateCourse';
-import Courses from './components/Courses/Courses';
+import Header from '../Header/Header';
+import CreateCourse from '../CreateCourse/CreateCourse';
+import Courses from '../Courses/Courses';
 
 import './App.css';
 
 import { Navigate, Route, Routes } from 'react-router-dom';
-import Registration from './components/Registration/Registration';
-import Login from './components/Login/Login';
-import CourseInfo from './components/CourseInfo/CourseInfo';
+import Registration from '../Registration/Registration';
+import Login from '../Login/Login';
+import CourseInfo from '../CourseInfo/CourseInfo';
 import { useDispatch, useSelector } from 'react-redux';
-import { selectAuthors, selectCourses } from './store/selectors';
-import getAuthors from './store/authors/thunk';
-import getCourses from './store/courses/thunk';
+import { selectAuthors, selectCourses } from '../../store/selectors';
+import getAuthors from '../../store/authors/thunk';
+import getCourses from '../../store/courses/thunk';
 
 function App() {
 	const dispatch = useDispatch();
