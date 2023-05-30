c6d298c89786c3163f6c99b2cccad09b2970f920
nick722 <79ZUdtd3orzC!GeTcb^s&79jBB>
2023-05-30 18:48:40 +0300

    make a post request to login

--- src/components/Login/Login.tsx
+++ src/components/Login/Login.tsx
@@ -1,9 +1,11 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Input from '../common/Input/Input';
 import { Link } from 'react-router-dom';
 
 import './Login.scss';
 import Button from '../common/Button/Button';
+import axios from 'axios';
+import { BASE_URL } from '../../services';
 
 const baseClass = 'login';
 const EMAIL_LABEL = 'Email';
@@ -13,30 +15,52 @@ const PASSWORD_PLACEHOLDER = 'Enter password';
 const LOGIN_BUTTON_TEXT = 'Login';
 
 const Login = () => {
+	const [email, setEmail] = useState('');
+	const [password, setPassword] = useState('');
+	const [loginError, setLoginError] = useState('');
+
+	const loginErrorMessage = `Login failed: ${loginError}`;
+
+	const login = async (event) => {
+		event.preventDefault();
+
+		const url = `${BASE_URL}/login`;
+
+		const userCreds = {
+			email: event.target.email?.value,
+			password: event.target.password?.value,
+		};
+
+		try {
+			const response = await axios.post(url, userCreds);
+			setLoginError('');
+		} catch (error) {
+			console.log('Axios error', error);
+			setLoginError(error.message);
+		}
+	};
+
 	return (
 		<div className={`${baseClass}`}>
 			<h1>Login</h1>
-			<form className={`${baseClass}__form`}>
+			<form onSubmit={(event) => login(event)} className={`${baseClass}__form`}>
 				<Input
 					labelText={EMAIL_LABEL}
 					placeholderText={EMAIL_PLACEHOLDER}
-					value=''
-					onChange={() => {
-						/**/
-					}}
-					name={EMAIL_LABEL}
+					value={email}
+					onChange={setEmail}
+					name='email'
 				/>
 				<Input
 					labelText={PASSWORD_LABEL}
 					placeholderText={PASSWORD_PLACEHOLDER}
-					value=''
-					onChange={() => {
-						/**/
-					}}
-					name={PASSWORD_LABEL}
+					value={password}
+					onChange={setPassword}
+					name='password'
 				/>
 				<Button buttonText={LOGIN_BUTTON_TEXT} />
 			</form>
+			<p className={`${baseClass}__error`}>{loginError && loginErrorMessage}</p>
 			<p>
 				If you don't have an account you can{' '}
 				<Link to={'/registration'}>Register</Link>
