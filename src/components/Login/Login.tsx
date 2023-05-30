83d6b5ee7613bc89e58598325460a2c1a11418c2
nick722 <79ZUdtd3orzC!GeTcb^s&79jBB>
2023-05-30 16:14:15 +0300

    style Login component

--- src/components/Login/Login.tsx
+++ src/components/Login/Login.tsx
@@ -1,7 +1,48 @@
 import React from 'react';
+import Input from '../common/Input/Input';
+import { Link } from 'react-router-dom';
+
+import './Login.scss';
+import Button from '../common/Button/Button';
+
+const baseClass = 'login';
+const EMAIL_LABEL = 'Email';
+const EMAIL_PLACEHOLDER = 'Enter email';
+const PASSWORD_LABEL = 'Password';
+const PASSWORD_PLACEHOLDER = 'Enter password';
+const LOGIN_BUTTON_TEXT = 'Login';
 
 const Login = () => {
-	return <div>Login component</div>;
+	return (
+		<div className={`${baseClass}`}>
+			<h1>Login</h1>
+			<form className={`${baseClass}__form`}>
+				<Input
+					labelText={EMAIL_LABEL}
+					placeholderText={EMAIL_PLACEHOLDER}
+					value=''
+					onChange={() => {
+						/**/
+					}}
+					name={EMAIL_LABEL}
+				/>
+				<Input
+					labelText={PASSWORD_LABEL}
+					placeholderText={PASSWORD_PLACEHOLDER}
+					value=''
+					onChange={() => {
+						/**/
+					}}
+					name={PASSWORD_LABEL}
+				/>
+				<Button buttonText={LOGIN_BUTTON_TEXT} />
+			</form>
+			<p>
+				If you don't have an account you can{' '}
+				<Link to={'/registration'}>Register</Link>
+			</p>
+		</div>
+	);
 };
 
 export default Login;
