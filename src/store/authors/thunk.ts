e4aa2bea51fc19a36e868a6965f59fb9f0f00a84
nick722 <79ZUdtd3orzC!GeTcb^s&79jBB>
2023-05-31 09:58:22 +0300

    commented out authors thunk

--- src/store/authors/thunk.ts
+++ src/store/authors/thunk.ts
@@ -1,21 +1,21 @@
-import axios from 'axios';
-// import { saveAuthorsAction } from './actions';
-import { BASE_URL } from '../../services';
-import { saveAuthors } from './authorsSlice';
-
-const getAuthors = () => async (dispatch, getState) => {
-	const url = `${BASE_URL}/authors/all`;
-
-	try {
-		const response = await axios.get(url);
-		const authors = response.data.result;
-		dispatch(saveAuthorsAction(authors));
-
-		// dispatch(saveAuthors(getState(), authors));
-		// dispatch(saveAuthors(authors));
-	} catch (error) {
-		console.log(error);
-	}
-};
-
-export default getAuthors;
+// import axios from 'axios';
+// // import { saveAuthorsAction } from './actions';
+// import { BASE_URL } from '../../services';
+// import { saveAuthors } from './authorsSlice';
+//
+// const getAuthors = () => async (dispatch, getState) => {
+// 	const url = `${BASE_URL}/authors/all`;
+//
+// 	try {
+// 		const response = await axios.get(url);
+// 		const authors = response.data.result;
+// 		dispatch(saveAuthorsAction(authors));
+//
+// 		// dispatch(saveAuthors(getState(), authors));
+// 		// dispatch(saveAuthors(authors));
+// 	} catch (error) {
+// 		console.log(error);
+// 	}
+// };
+//
+// export default getAuthors;
