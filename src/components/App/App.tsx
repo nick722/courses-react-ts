615f54cf062a5ea109a1ea319bcdbebe7f15c89c
nick722 <79ZUdtd3orzC!GeTcb^s&79jBB>
2023-05-31 09:43:55 +0300

    set Login Route as default

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -54,10 +54,7 @@ function App() {
 						path={AppRoutes.CREATE_COURSE}
 						element={<CreateCourse allAuthors={authors} />}
 					/>
-					<Route
-						path='/'
-						element={<Navigate to={AppRoutes.COURSES} replace />}
-					/>
+					<Route path='/' element={<Navigate to={AppRoutes.LOGIN} replace />} />
 				</Route>
 			</Routes>
 		</div>
